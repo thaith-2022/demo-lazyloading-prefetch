78ff5d3b31bf26b8b2d8d01b37c694565f57a8fa
Truong Hong Thai <truonghongthai97@gmail.com>
2020-12-22 22:18:43 +0700

    demo

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
+// import HomePage from "./HomePage";
+// import AboutPage from "./AboutPage";
 
-Vue.config.productionTip = false
+const HomePage = () => import(/* webpackChunkName: "HomePage" */ './HomePage')
+const AboutPage = () => import(/* webpackChunkName: "AboutPage" */ './AboutPage')
+
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
+const routes = [
+  { path: "/", component: HomePage },
+  { path: "/about", component: AboutPage },
+];
+
+const router = new VueRouter({
+  routes, // short for `routes: routes`
+  mode: 'history',
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  router,
+}).$mount("#app");
